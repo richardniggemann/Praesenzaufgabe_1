04cc23d36d0467f1c1af7d7f558e108abf8cd99a
Richard Niggemann <rniggema@smail.uni-koeln.de>
2025-10-27 13:00:21 +0100

    other solution for homework task 2

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -1,14 +1,13 @@
 package h2;
 
-import java.util.List;
-import java.util.stream.Stream;
+import java.util.Arrays;
 
 public class H2_main {
     public static void main(String[] args) {
         int i = 0, j = 1, k = 2, min, max;
-
-        List<Integer> values = Stream.of(i, j, k).sorted().toList();
-        min = values.getFirst();
-        max = values.getLast();
+        int[] values = {i, j, k};
+        Arrays.sort(values);
+        min = values[0];
+        max = values[values.length - 1];
     }
 }
