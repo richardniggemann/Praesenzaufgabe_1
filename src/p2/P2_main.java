d15bb83e1c7820cecc009271357383125a36d243
Richard Niggemann <rniggema@smail.uni-koeln.de>
2025-10-27 12:30:44 +0100

    solve task 2

--- src/p2/P2_main.java
+++ src/p2/P2_main.java
@@ -1,4 +1,13 @@
 package p2;
 
 public class P2_main {
+    public static void main(String[] args) {
+        int i = 15;
+        boolean b = false;
+
+        if (i > 10) {
+            b = true;
+        }
+        System.out.println(b);
+    }
 }
