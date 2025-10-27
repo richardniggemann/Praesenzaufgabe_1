bed5cc13271c4093279bdd04c3f4b3dd2af843fa
Richard Niggemann <rniggema@smail.uni-koeln.de>
2025-10-27 13:12:04 +0100

    solve homework task 3

---
+++ src/h3/H3_main.java
@@ -0,0 +1,23 @@
+package h3;
+
+public class H3_main {
+    public static void main(String[] args) {
+        int i = 0, j = 1, k = -10;
+        boolean expression1 = i > j;
+        boolean expression2 = i > 200;
+        boolean expression3 = j > 100;
+
+        if (expression1 && !expression2 && !expression3) {
+            k = 1;
+        }
+        if (expression1 && expression2 && !expression3) {
+            k = 2;
+        }
+        if (expression1 && expression2 && expression3) {
+            k = 3;
+        }
+        if (!expression1 && !expression2 && !expression3) {
+            k = 4;
+        }
+    }
+}
