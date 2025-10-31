27c486f13b90e804d7144dfaa98efd18b54329a3
Richard Niggemann <rniggema@smail.uni-koeln.de>
2025-10-31 09:22:51 +0100

    extract logic

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -2,10 +2,12 @@ package h1;
 
 public class H1_main {
     public static void main(String[] args) {
-        int z = 18;
-        if (z > 10) {
-            z -= 10;
-        }
+        int z = 42;
+        z = conditionalSubstraction(z);
         System.out.println(z);
+    }
+
+    private static int conditionalSubstraction(int z) {
+        return z > 10 ? z - 10 : z;
     }
 }
